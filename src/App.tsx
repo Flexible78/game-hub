4fac41487ef24da41b989021caa079ebfa68078f
Alexander <alexandertsyrkin@gmail.com>
2026-02-22 12:32:12 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Grid, GridItem, Stack } from "@chakra-ui/react"
+import Nav from "./components/Nav"
+import GameGrid from "./components/GameGrid"
+
+
+function App() {
+
+
+    return (
+        <Grid templateAreas={
+            {
+                base: '"nav" "main"',
+                md: '"nav nav" "aside main"  '
+            }
+        }>
+            <GridItem area="nav" bg="aliceblue">
+                <Nav></Nav>
+            </GridItem>
+            <Stack hideBelow={"md"}>
+                <GridItem area="aside" bg='gold' >
+                    ASIDE
+                </GridItem>
+            </Stack>
+            <GridItem area="main" >
+                <GameGrid/>
+            </GridItem>
+
+        </Grid>
+    )
+}
+
+export default App
