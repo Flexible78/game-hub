db05b765bb81c719be44efd9a6491e0526afeaab
Alexander <alexandertsyrkin@gmail.com>
2026-02-25 08:32:05 +0200

    fix api problem

--- src/services/app-client.ts
+++ src/services/app-client.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance } from "axios";
 const apiClient: AxiosInstance = axios.create({
     baseURL: "https://api.rawg.io/api/",
     params: {
-        key: "7f7d4bdcf46e46539597864da68574a1"
+        key: "c5ee0c99a2f44bc2813d961b44538bc4"
     }
 });
 export default apiClient;
