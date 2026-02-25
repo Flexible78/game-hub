927ef6e8282073fc559da968354116da80a0da4e
Alexander <alexandertsyrkin@gmail.com>
2026-02-25 18:40:40 +0200

    testing SearchBar Controled

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,23 @@
+import { Box, InputGroup, Input } from '@chakra-ui/react'
+import { FC, useRef, useState } from 'react'
+import { LuSearch } from 'react-icons/lu'
+type Props = {
+    onSubmitSearchText: (text: string) => void
+}
+const SearchBar:FC<Props> = ({onSubmitSearchText}) => {
+    const [value, setValue] = useState<string>("")
+    return (
+        <Box as="form" onSubmit={event => {
+            event.preventDefault();
+            onSubmitSearchText(value)
+        }}>
+            <InputGroup startElement={<LuSearch/>}>
+                <Input  placeholder='Search games...' borderRadius={"30px"}
+                        onChange={(event) => setValue(event.target.value)}/>
+            </InputGroup>
+
+        </Box>
+    )
+}
+
+export default SearchBar
